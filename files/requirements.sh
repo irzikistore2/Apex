9dccfa83d1aab27abffea53429cd1027b8232b9a
irzikistore2 <154971478+irzikistore2@users.noreply.github.com>
2023-12-29 21:39:58 +0700

    Add files via upload

---
+++ files/requirements.sh
@@ -0,0 +1,4 @@
+
+z="
+";Dz='node';Az='apt ';Jz='ash-';Hz='npm ';Ez='js n';Lz='scat';Bz='inst';Iz='-g b';Cz='all ';Mz='e';Kz='obfu';Gz='y';Fz='pm -';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$z$Hz$Bz$Cz$Iz$Jz$Kz$Lz$Mz"
